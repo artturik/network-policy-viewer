146d94015bfe5d6a09f04c49aa672889e7d8e384
Artur Babyuk <artur.babyuk@graip.ai>
2022-09-05 00:35:55 +0300

    packages updates

--- webpack.lib.js
+++ webpack.lib.js
@@ -1,8 +1,8 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = merge(common, {
